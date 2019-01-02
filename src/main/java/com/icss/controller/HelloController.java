4f3167c448b9b57e8d65c5ba0688233aeede21a8
xylocarp <2803824269@qq.com>
2019-01-02 14:10:19 +0800

    初始化

---
+++ src/main/java/com/icss/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.icss.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String say(){
+		return "/hello.jsp";
+	}
+}
